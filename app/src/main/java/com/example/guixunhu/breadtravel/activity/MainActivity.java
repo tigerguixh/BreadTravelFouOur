743ab797589f513be7288de6e46abf84272d2843
guixunhu <guixunhu@letv.com>
2015-08-16 20:00:50 +0800

    add a androidannoation library

---
+++ app/src/main/java/com/example/guixunhu/breadtravel/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.guixunhu.breadtravel.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.guixunhu.breadtravel.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
