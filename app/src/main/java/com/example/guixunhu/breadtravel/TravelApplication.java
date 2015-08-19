9ec18323d03eaa5f22539aae8b2b11a23ce94b36
guixunhu <guixunhu@letv.com>
2015-08-19 22:15:49 +0800

    add a androidannoation library

---
+++ app/src/main/java/com/example/guixunhu/breadtravel/TravelApplication.java
@@ -0,0 +1,10 @@
+package com.example.guixunhu.breadtravel;
+
+import android.app.Application;
+
+/**
+ * Created by guixunhu on 15/8/16.
+ */
+public class TravelApplication extends Application {
+
+}
